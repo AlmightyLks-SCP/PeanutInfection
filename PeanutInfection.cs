c731ed566d1135d13992904161c751a5b4f28374
almightylks <lukasbarber@web.de>
2020-11-21 18:41:37 +0100

    Disabled mtf/chaos spawning & disabled decontamination

--- PeanutInfection.cs
+++ PeanutInfection.cs
@@ -7,10 +7,10 @@ namespace PeanutInfection
 {
     public class PeanutInfection : IGamemode
     {
-        public string Name { get; set; } 
-        public string Author { get; set; } 
-        public string GitHubRepo { get; set; } 
-        public string Version { get; set; } 
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string GitHubRepo { get; set; }
+        public string Version { get; set; }
         public PeanutInfection()
         {
             Author = "AlmightyLks";
@@ -23,13 +23,18 @@ namespace PeanutInfection
         {
             Synapse.Api.Events.EventHandler.Get.Round.SpawnPlayersEvent += Round_SpawnPlayersEvent;
             Synapse.Api.Events.EventHandler.Get.Player.PlayerDeathEvent += Player_PlayerDeathEvent;
+            Synapse.Api.Events.EventHandler.Get.Map.LCZDecontaminationEvent += Map_LCZDecontaminationEvent;
+            Synapse.Api.Events.EventHandler.Get.Player.PlayerSetClassEvent += Player_PlayerSetClassEvent;
 
             SynapseController.Server.Logger.Info("PeanutInfection Started");
         }
+
         public void End()
         {
             Synapse.Api.Events.EventHandler.Get.Round.SpawnPlayersEvent -= Round_SpawnPlayersEvent;
             Synapse.Api.Events.EventHandler.Get.Player.PlayerDeathEvent -= Player_PlayerDeathEvent;
+            Synapse.Api.Events.EventHandler.Get.Map.LCZDecontaminationEvent -= Map_LCZDecontaminationEvent;
+            Synapse.Api.Events.EventHandler.Get.Player.PlayerSetClassEvent -= Player_PlayerSetClassEvent;
 
             SynapseController.Server.Logger.Info("PeanutInfection Ended");
         }
@@ -52,5 +57,12 @@ namespace PeanutInfection
             if (ev.Victim.RoleID != 0)
                 ev.Victim.RoleID = (int)RoleType.Scp173;
         }
+        private void Player_PlayerSetClassEvent(PlayerSetClassEventArgs ev)
+        {
+            if (ev.Role != RoleType.ClassD && ev.Role != RoleType.Scp173)
+                ev.Allow = false;
+        }
+        private void Map_LCZDecontaminationEvent(LCZDecontaminationEventArgs ev)
+            => ev.Allow = false;
     }
 }
